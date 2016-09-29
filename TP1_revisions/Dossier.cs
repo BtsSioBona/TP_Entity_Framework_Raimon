2dc44bc6e634b14b56e66dfd361284945652242c
Hirokilen <dylanraimon@gmail.com>
2016-09-29 09:46:05 +0200

    commit fin classe XmlRead

---
+++ TP1_revisions/Dossier.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesMetier
+{
+    public class Dossier
+    {
+        // Attribut privée  de la classe Dossier
+        public string NomPatient { get; set; }
+        public string PrenomPatient { get; set; }
+        public DateTime DateNaissancePatient { get; set; }
+        public List<Prestation> MesPrestations { get; set; }
+
+        // Constructeur de la classe Dossier
+        public Dossier(string pNomPatient, string pPrenomPatient, DateTime pDateNaissancePatient)
+        {
+            this.NomPatient = pNomPatient;
+            this.PrenomPatient = pPrenomPatient;
+            this.DateNaissancePatient = pDateNaissancePatient;
+            this.MesPrestations = new List<Prestation>();
+        }
+
+        public Dossier(string pNomPatient, string pPrenomPatient, DateTime pDateNaissancePatient, Prestation unePrestation)
+        {
+            this.NomPatient = pNomPatient;
+            this.PrenomPatient = pPrenomPatient;
+            this.DateNaissancePatient = pDateNaissancePatient;
+            this.MesPrestations = new List<Prestation>();
+            this.MesPrestations.Add(unePrestation);
+        }
+
+        public Dossier(string pNomPatient, string pPrenomPatient, DateTime pDateNaissancePatient, List<Prestation> pLesPrestations)
+        {
+            this.NomPatient = pNomPatient;
+            this.PrenomPatient = pPrenomPatient;
+            this.DateNaissancePatient = pDateNaissancePatient;
+            this.MesPrestations = new List<Prestation>();
+            this.MesPrestations = pLesPrestations;
+        }
+
+        // Methodes publiques 
+
+        public override string ToString()
+        {
+            return "Dossier de " + NomPatient + " " + PrenomPatient + " née en " + DateNaissancePatient + " nombre de prestation : "+ this.MesPrestations.Count;
+        }
+        // ajoute une prestation dans la collection mesPrestations
+        public void ajoutePrestation(string pLibelle, DateTime pDate, DateTime pHeure, Intervenant pIntervenant)
+        {
+            MesPrestations.Add(new Prestation(pLibelle, pDate, pHeure, pIntervenant));
+        }
+
+        // retourne le nombre d'intervenant externe dans la collection mesPrestation
+        public int getNbPrestationsExternes()
+        {
+            int nb = 0;
+            foreach (Prestation Prestation in MesPrestations)
+            {
+                if(Prestation.I_Intervenant is IntervenantExterne)
+                {
+                    nb++;
+                }
+            }
+            return nb;
+
+        }
+
+        // retourne le nombre de prestations
+        public int getNbPrestations()
+        {
+            return MesPrestations.Count();
+        }
+
+        public int getNbJoursSoins()
+        {
+            List<DateTime> listeDate = new List<DateTime>();
+                foreach (Prestation unePrestation in MesPrestations)
+                {
+                    if (!(listeDate.Contains(unePrestation.DateSoin)))
+                    {
+                        listeDate.Add(unePrestation.DateSoin);
+                    }
+                }
+                return listeDate.Count();
+            }
+        }
+    }
+
---
+++ TP1_revisions/Prestation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesMetier
+{
+    public class Prestation
+    {
+        public string Libelle { get; }
+
+        public DateTime DateSoin { get; }
+
+        public DateTime Heuresoin { get; }
+
+        public Intervenant I_Intervenant { get; }
+
+        public Prestation(string pLibelle, DateTime pDateSoin, DateTime pHeureSoin, Intervenant pI_Intervenant)
+        {
+            this.Libelle = pLibelle;
+            this.DateSoin = pDateSoin;
+            this.Heuresoin = pHeureSoin;
+            this.I_Intervenant = pI_Intervenant;
+        }
+
+        public int compareTo(Prestation unePrestation)
+        {
+            if( this.DateSoin.Year == unePrestation.DateSoin.Year && this.DateSoin.Month == unePrestation.DateSoin.Month && this.DateSoin.Day == unePrestation.DateSoin.Day)
+            {
+                return 0;
+            }
+            else if(this.DateSoin.Year > unePrestation.DateSoin.Year && this.DateSoin.Month > unePrestation.DateSoin.Month && this.DateSoin.Day > unePrestation.DateSoin.Day)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return ("Libelle : " + this.Libelle + "\n DateSoin : " + this.DateSoin + "\n HeureSoin : " + this.Heuresoin + "\n Intervenant : " + this.I_Intervenant);
+        }
+    }
+}
